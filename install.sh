ef308bd64646326adc082258aff6b481bd622f20
Marques Robinson <marques.j.robinson@gmail.com>
2020-05-09 21:36:24 -0500

    create install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Homebrew & Packages
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew install coreutils
+brew install zsh
+brew install git
+brew install vim
+brew install tmux
+
+# Oh My Zsh
+/bin/bash -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
