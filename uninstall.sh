6ed0d24b689c625fe173df15b71b64316ca6e590
Marques Robinson <marques.j.robinson@gmail.com>
2020-05-09 23:50:27 -0500

    update uninstall script to include all artifacts to be removed, also add exit to the end to force the shell to exit when done

--- uninstall.sh
+++ uninstall.sh
@@ -3,9 +3,15 @@
 # Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)"
 
-# Clean up zsh artifacts
+# Remove artifacts
 rm -rf .oh-my-zsh
 rm .zshrc
+rm .gitconfig && rm .gitignore
+rm .vimrc
+rm .tmux.conf
 
-# Set bash
+# Re-enable bash as default shell
 chsh -s /bin/bash
+
+# Force exit
+exit
