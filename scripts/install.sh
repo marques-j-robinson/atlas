42b724c2cf68f696146a206468c7b2ac76de530e
Marques Robinson <marques.j.robinson@gmail.com>
2020-05-14 11:46:28 -0500

    add step for installing oh my zsh

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,5 +8,17 @@ source $UTILITY_PATH/utilities.sh
 # 1. Git
 
 ## Create symbol links between gitconfig and gitignore files for global settings
+echo "Setting up global Git settings"
 symlink git gitconfig
 symlink git gitignore
+
+# 2. Install Oh-My-Zsh
+
+ZSH=~/.oh-my-zsh
+
+if [ -d $ZSH ]; then
+    echo "Oh My Zsh is already installed. Skipping..."
+else
+    echo "Installing Oh My Zsh..."
+    curl -L http://install.ohmyz.sh | sh
+fi
