0d6eec944c786376484bb9fbc50b6c10ac5cc05b
marques-j-robinson <marques.j.robinson@gmail.com>
2021-04-04 16:59:19 -0700

    update homebrew install script and remove Brewfile

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -7,4 +7,33 @@ then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+brew update
+brew upgrade
+
+# Those that come with macOS are outdated
+brew install coreutils
+
+# Development
+brew install \
+    awscli \
+    git \
+    neovim \
+    node \
+    python \
+    tmux \
+    yarn \
+    zsh    
+
+# Applications
+brew install --cask \
+    1password \
+    1password-cli \
+    firefox \
+    google-chrome \
+    iterm2 \
+    slack \
+    zoom
+
+brew cleanup
+
 exit 0
